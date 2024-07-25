970b4725a56f17d6f6c109d79cdad1943abed2fe
Sabari K <sabari8956@gmail.com>
2024-07-26 00:10:31 +0530

    organization, Tournament, Category models created

--- organization/models.py
+++ organization/models.py
@@ -1,3 +1,41 @@
 from django.db import models
-
+from core.models import User
 # Create your models here.
+class Organization(models.Model):
+    name = models.CharField(max_length=255)
+    ph_number = models.CharField(max_length=10)
+    mail = models.EmailField(max_length=255)
+    admin = models.ForeignKey(User, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.name
+
+class Tournament(models.Model):
+    name = models.CharField(max_length=255)
+    details = models.TextField()
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    venue_address = models.CharField(max_length=1024)
+    venue_link = models.URLField(max_length=512)
+    categories = models.ManyToManyField('Category', blank=True)
+    
+    # things to be added
+        # poster
+        # courts
+        
+    def __str__(self):
+        return f"{self.name} - {self.organization.name}"
+    
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    details = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    tournament = models.ForeignKey(Tournament, on_delete=models.CASCADE)
+    registration_status = models.BooleanField(default=True)
+    
+    # things to be added
+        # winner
+        # teams
+        # fixture type
+        
