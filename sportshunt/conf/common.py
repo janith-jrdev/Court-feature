3dc61253e488ed6bbf7ac3682427ee05f68be1c2
Sabari K <sabari8956@gmail.com>
2024-10-23 10:28:58 +0000

    updated setting for prod

--- sportshunt/conf/common.py
+++ sportshunt/conf/common.py
@@ -30,6 +30,7 @@ INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
